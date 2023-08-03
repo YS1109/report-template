465d63b20168ac681a681fe283401a3c6e944c80
YS <35719286+YS1109@users.noreply.github.com>
2023-08-04 00:16:22 +0800

    init

---
+++ server/report-template/src/main/java/com/ysoztf/reporttemplate/ReportTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ysoztf.reporttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportTemplateApplication.class, args);
+    }
+
+}
---
+++ server/report-template/src/test/java/com/ysoztf/reporttemplate/ReportTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysoztf.reporttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
